bbe569050c9f987bb8bb81f09cbbb32d6dc22d7c
Makss Golubs <makss.golubs@ibm.com>
2020-07-02 23:26:16 +0200

    Initial datastructure

---
+++ src/Athlete.java
@@ -0,0 +1,18 @@
+public class Athlete {
+
+    private final int id;
+    private String name;
+    private int birthyear;
+    private String gender;
+    private int height;
+    private int weight;
+
+    Athlete(int id, String name, int birthyear, String gender, int height, int weight) {
+        this.id = id;
+        this.name = name;
+        this.birthyear = birthyear;
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight;
+    }
+}
