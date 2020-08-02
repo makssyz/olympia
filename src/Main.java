47ed23c57efdd476ff03bb3e4b0ca3038b5b1be9
Makss Golubs <makss.golubs@ibm.com>
2020-08-02 13:08:27 +0200

    Add Disclaimer and UML Diagrams

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+/* DISCLAIMER
+ * This code has been fully designed and written by Makss Golubs during the second semester of the Computational
+ * Data Science course at the Cooperative State University Baden Württemberg. A few code snippets from the internet
+ * have been used as inspiration. Any resemblance to the work of other students from the same course is either a
+ * coincidence, or (in case it's not credited) an act of plagiarism of the other party. The whole development process
+ * can be looked up and retraced on https://github.com/makssyz/olympia as proof in case of doubt.
+ */
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
